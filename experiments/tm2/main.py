57b9b3021f2cd969ac44c3d326fcf1fee570fe40
Clari <gonellcl@gmail.com>
2024-04-15 15:07:48 -0400

    Update main.py

--- experiments/tm2/main.py
+++ experiments/tm2/main.py
@@ -28,20 +28,24 @@ def set_seed(seed):
 
 def get_config():
     parser = get_public_config()
-    parser.add_argument('--input_dim', type=int, default=32)
-    parser.add_argument('--output_dim', type=int, default=32)
-    parser.add_argument('--hid_dim', type=int, default=64)
+    parser.add_argument('--init_dim', type=int, default=3)
+    parser.add_argument('--out_dim', type=int, default=1)
+    parser.add_argument('--h_dim', type=int, default=64)
+    parser.add_argument('--node_num', type=int, default=716),
+    # parser.add_argument('--input_dim', type=int, default=32),
+    # parser.add_argument('--output_dim', type=int, default=32),
+    parser.add_argument('--layer', type=int, default=2)
+    parser.add_argument('--dropout', type=float, default=0.1)
+    parser.add_argument('--end_dim', type=int, default=512)
 
-    parser.add_argument('--num_clusters', type=int, default=32)
+    parser.add_argument('--num_clusters', type=int, default=96)
     parser.add_argument('--filter_type', type=str, default='doubletransition')
 
     # parser.add_argument('--seq_len', type=int, default=64)
     # parser.add_argument('--horizon', type=int, default=512)
 
-
     parser.add_argument('--lrate', type=float, default=1e-3)
     parser.add_argument('--wdecay', type=float, default=1e-4)
-    parser.add_argument('--dropout', type=float, default=0.1)
     parser.add_argument('--clip_grad_value', type=float, default=5)
     args = parser.parse_args()
 
@@ -53,6 +57,8 @@ def get_config():
 
 
 def main():
+    torch.autograd.set_detect_anomaly(True)  # Enable anomaly detection
+
     args, log_dir, logger = get_config()
     set_seed(args.seed)
     device = torch.device(args.device)
@@ -61,18 +67,31 @@ def main():
     logger.info('Adj path: ' + adj_path)
 
     adj_mx = load_adj_from_numpy(adj_path)
+    adj_mx = adj_mx - np.eye(node_num)
+    adj_mx = np.zeros((node_num, node_num), dtype=np.float32)
+    for n in range(node_num):
+        idx = np.nonzero(adj_mx[n])[0]
+        adj_mx[n, idx] = 1
+    adj_mx = normalize_adj_mx(adj_mx, 'scalap')[0]
+    adj_mx = torch.tensor(adj_mx).to(device)
+
+    logger.info(f'Shape of Adj_Matrix {adj_mx.shape}')
 
     dataloader, scaler = load_dataset(data_path, args, logger)
 
-    model = GMGNN(input_dim=args.input_dim,
-                     output_dim=args.output_dim,
-                     adj_mx=adj_mx,
-                     filter_type=args.filter_type,
-                     hid_dim=args.hid_dim,
-                     dropout=args.dropout,
-                     num_clusters=args.num_clusters,
+    model = GMGNN(node_num=node_num,
+                  input_dim=args.input_dim,
+                  init_dim=args.init_dim,
+                  output_dim=args.output_dim,
+                  adj_mx=adj_mx,
+                  h_dim=args.h_dim,
+                  num_clusters=args.num_clusters,
+                  dropout=args.dropout,
+                  end_dim=args.end_dim,
+                  layer=args.layer,
+                  device=args.device,
 
-                     )
+                  )
 
     loss_fn = masked_mae
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lrate, weight_decay=args.wdecay)
